3b8e2f55df222cc98fa6bb85154899ab5f29363b
QaManual88 <fev215@proton.me>
2025-01-03 19:53:12 +0200

    HW8

---
+++ 2.py
@@ -0,0 +1,9 @@
+def is_palindrome(s):
+    s = ''.join(char.lower() for char in s if char.isalnum())
+    return s == s[::-1]
+
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("ОК")
